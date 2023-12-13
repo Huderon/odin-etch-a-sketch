060ed80bf5014346537cc23381e4d2535e539596
Huderon <huderon.mcs@gmail.com>
2023-12-13 19:51:11 +1100

    Add event listeners and functionality for mouse events

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
+const RESOLUTION = 16;
+const DEFAULT_COLOR = "#000000";
 const canvas = document.querySelector(".etch-a-sketch__canvas");
+canvas.addEventListener("mouseover", handleMouseEvents);
+canvas.addEventListener("mouseout", handleMouseEvents);
+canvas.addEventListener("mousedown", handleMouseEvents);
 
 function createGrid(rows) {
     const fragment = document.createDocumentFragment();
+    const pixelWidth = 100 / rows;
     for (let i = 0; i < Math.pow(rows, 2); i++) {
         const pixel = document.createElement("div");
         pixel.className = "etch-a-sketch__pixel";
-        pixel.style.width = `${100 / rows}%`;
+        pixel.style.width = `${pixelWidth}%`;
         fragment.appendChild(pixel);
     }
+    canvas.innerHTML = "";
     canvas.appendChild(fragment);
 }
 
-createGrid(64);
+function handleMouseEvents(e) {
+    if (!e.target.classList.contains("etch-a-sketch__pixel")) return;
+
+    switch (e.type) {
+        case "mouseover":
+            if (e.buttons === 1) {
+                e.target.style.backgroundColor = DEFAULT_COLOR;
+                e.target.classList.add("etch-a-sketch__pixel--active");
+            } else {
+                originalColor = window.getComputedStyle(e.target).backgroundColor;
+                e.target.style.backgroundColor = DEFAULT_COLOR;
+            }
+            break;
+        case "mouseout":
+            if (e.target.classList.contains("etch-a-sketch__pixel--active"))
+                return;
+            e.target.style.backgroundColor = originalColor;
+            break;
+        case "mousedown":
+            if (e.buttons !== 1) return;
+            e.target.style.backgroundColor = DEFAULT_COLOR;
+            e.target.classList.add("etch-a-sketch__pixel--active");
+            break;
+    }
+}
+
+createGrid(RESOLUTION);
