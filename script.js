fff73639a16f10775c63825d075e3dc81c41cd96
Huderon <huderon.mcs@gmail.com>
2025-09-09 17:30:12 +1000

    implement opacity slider

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const RESOLUTION = 16;
 const form = document.getElementById("controls");
 const colourPicker = form.elements["colour-picker"];
+const opacitySlider = form.elements["opacity-slider"];
 const drawModeRadioGroup = form.elements["draw-mode"];
 const resetButton = form.elements["reset-button"];
 const canvas = document.querySelector(".etch-a-sketch__canvas");
@@ -23,8 +24,8 @@ function createGrid(rows) {
         pixel.className = "etch-a-sketch__pixel";
         pixel.style.width = `${pixelWidth}%`;
         pixelColours.set(pixel, {
-            colour: null,
-            previewColour: null
+            colour: {r: 255, g: 255, b: 255, a: 1},
+            previewColour: {r: 255, g: 255, b: 255, a: 1}
         });
         fragment.appendChild(pixel);
     }
@@ -42,20 +43,55 @@ function resetGrid() {
     createGrid(rows);
 }
 
-function getColour() {
+function hexToRGBA(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+        a: parseFloat(opacitySlider.value)
+    } : null;
+}
+
+function generateRGBA(a) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return {r, g, b, a}
+}
+
+function RGBAToString({r, g, b, a}) {
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function compositeRGBA(fg, bg) {
+    if (bg === null) return fg;
+
+    const a = fg.a + bg.a * (1 - fg.a);
+
+    const r = Math.round((fg.r * fg.a + bg.r * bg.a * (1 - fg.a)) / a);
+    const g = Math.round((fg.g * fg.a + bg.g * bg.a * (1 - fg.a)) / a);
+    const b = Math.round((fg.b * fg.a + bg.b * bg.a * (1 - fg.a)) / a);
+
+    return {r, g, b, a};
+}
+
+function getColour(colour) {
+    let RGBA;
     switch (drawModeRadioGroup.value) {
         case "erase":
-            return "";
+            RGBA = {r: 255, g: 255, b: 255, a: parseFloat(opacitySlider.value)};
+            break;
         case "random":
-            const r = Math.floor(Math.random() * 256);
-            const g = Math.floor(Math.random() * 256);
-            const b = Math.floor(Math.random() * 256);
-            return `rgb(${r}, ${g}, ${b})`;
+            RGBA = generateRGBA(parseFloat(opacitySlider.value));
+            break;
         case "static":
-            return colourPicker.value;
+            RGBA = hexToRGBA(colourPicker.value);
+            break;
         default:
             break;
     }
+    return compositeRGBA(RGBA, colour)
 }
 
 function handleMouseEvents(e) {
@@ -64,19 +100,19 @@ function handleMouseEvents(e) {
     const pixelState = pixelColours.get(pixel);
     switch (e.type) {
         case "mouseover":
-            pixelState.previewColour = getColour();
-            pixel.style.backgroundColor = pixelState.previewColour;
+            pixelState.previewColour = getColour(pixelState.colour);
+            pixel.style.backgroundColor = RGBAToString(pixelState.previewColour);
             if (e.buttons === 1) {
                 pixelState.colour = pixelState.previewColour;
             }
             break;
         case "mouseout":
-            pixel.style.backgroundColor = pixelState.colour;
+            pixel.style.backgroundColor = RGBAToString(pixelState.colour);
             break;
         case "mousedown":
             if (e.buttons === 0) return;
             pixelState.colour = pixelState.previewColour;
-            pixel.style.backgroundColor = pixelState.colour;
+            pixel.style.backgroundColor = RGBAToString(pixelState.colour);
     }
 }
 
